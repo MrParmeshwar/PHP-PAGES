7003d761d46aa4792365bebf9ea03c3585d7e705
MrParmeshwar <126458965+MrParmeshwar@users.noreply.github.com>
2023-09-07 15:15:18 +0530

    Add files via upload

---
+++ Date.php
@@ -0,0 +1,7 @@
+<?php
+echo ("Program For Date & Time with Different Format.<br>");
+  echo date("d-m-y l h:i:s a")."<br>";
+  echo date("d:m:y l h:i:s a")."<br>";
+  echo date("d/m/y l h:i:s a")."<br>";
+
+?>
